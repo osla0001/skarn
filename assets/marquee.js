339c0e0cd575f7d219e1d1c6e0776fe4a081cbc7
Oscar Laursen <osla0001@stud.kea.dk>
2026-02-02 17:02:27 +0100

    kollektion kort-flex færdige

--- assets/marquee.js
+++ assets/marquee.js
@@ -1,5 +1,5 @@
-import { Component } from '@theme/component';
-import { debounce } from '@theme/utilities';
+import { Component } from "@theme/component";
+import { debounce } from "@theme/utilities";
 
 const ANIMATION_OPTIONS = {
   duration: 500,
@@ -16,7 +16,7 @@ const ANIMATION_OPTIONS = {
  * @extends Component<Refs>
  */
 class MarqueeComponent extends Component {
-  requiredRefs = ['wrapper', 'content', 'marqueeItems'];
+  requiredRefs = ["wrapper", "content", "marqueeItems"];
 
   async connectedCallback() {
     super.connectedCallback();
@@ -33,16 +33,16 @@ class MarqueeComponent extends Component {
 
     this.#setSpeed(speed);
 
-    window.addEventListener('resize', this.#handleResize);
-    this.addEventListener('pointerenter', this.#slowDown);
-    this.addEventListener('pointerleave', this.#speedUp);
+    window.addEventListener("resize", this.#handleResize);
+    this.addEventListener("pointerenter", this.#slowDown);
+    this.addEventListener("pointerleave", this.#speedUp);
   }
 
   disconnectedCallback() {
     super.disconnectedCallback();
-    window.removeEventListener('resize', this.#handleResize);
-    this.removeEventListener('pointerenter', this.#slowDown);
-    this.removeEventListener('pointerleave', this.#speedUp);
+    window.removeEventListener("resize", this.#handleResize);
+    this.removeEventListener("pointerenter", this.#slowDown);
+    this.removeEventListener("pointerleave", this.#speedUp);
   }
 
   /**
@@ -105,7 +105,7 @@ class MarqueeComponent extends Component {
    * @param {number} value
    */
   #setSpeed(value) {
-    this.style.setProperty('--marquee-speed', `${value}s`);
+    this.style.setProperty("--marquee-speed", `${value}s`);
   }
 
   async #queryNumberOfCopies() {
@@ -136,7 +136,7 @@ class MarqueeComponent extends Component {
             });
           }, 0);
         },
-        { root: this }
+        { root: this },
       );
       intersectionObserver.observe(marqueeItems[0]);
     });
@@ -146,7 +146,7 @@ class MarqueeComponent extends Component {
    * @param {number} numberOfCopies
    */
   #calculateSpeed(numberOfCopies) {
-    const speedFactor = Number(this.getAttribute('data-speed-factor'));
+    const speedFactor = Number(this.getAttribute("data-speed-factor"));
     const speed = Math.sqrt(numberOfCopies) * speedFactor;
 
     return speed;
@@ -188,8 +188,8 @@ class MarqueeComponent extends Component {
 
     const clone = /** @type {HTMLElement} */ (this.refs.content.cloneNode(true));
 
-    clone.setAttribute('aria-hidden', 'true');
-    clone.removeAttribute('ref');
+    clone.setAttribute("aria-hidden", "true");
+    clone.removeAttribute("ref");
 
     this.refs.wrapper.appendChild(clone);
   }
@@ -254,7 +254,7 @@ function animateValue({ from, to, duration, onUpdate, easing = (t) => t * t * (3
 
     if (progress < 1) {
       requestAnimationFrame(animate);
-    } else if (typeof onComplete === 'function') {
+    } else if (typeof onComplete === "function") {
       onComplete();
     }
   }
@@ -271,6 +271,6 @@ function animateValue({ from, to, duration, onUpdate, easing = (t) => t * t * (3
   };
 }
 
-if (!customElements.get('marquee-component')) {
-  customElements.define('marquee-component', MarqueeComponent);
+if (!customElements.get("marquee-component")) {
+  customElements.define("marquee-component", MarqueeComponent);
 }
